4d8bdc069fa1759105fb0d207ce23c2d0cf51d9e
sachin6005 <85541761+sachin6005@users.noreply.github.com>
2023-01-11 02:16:15 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,9 +8,3 @@ terraform {
 
   required_version = ">= 1.2.0"
 }
-
-provider "aws" {
-  access_key = "AKIA22F4LSIK4R6Z376X"
-  secret_key = "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"
-  region     = "ap-south-2"
-}
